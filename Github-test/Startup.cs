477c6c7f92adbc4177fb1021479e27b4d716a6e9
DeepikaS <deepikaswins@gmail.com>
2019-10-15 17:13:41 +0530

    Add project files.

---
+++ Github-test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Github_test.Startup))]
+namespace Github_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
